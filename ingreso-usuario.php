59222ffc9a3143ab47acd6dbd0cf849bcb8a393d
web5 <web5@ayla.dt>
2020-09-24 10:25:57 +0200

    variables id y pedido

--- ingreso-usuario.php
+++ ingreso-usuario.php
@@ -1,21 +1,29 @@
 <?php 
 // recogemos datos del formulario e insertamos en la base de datos
 
-// var_dump($_POST);
-
 $nombre_usuario = $_POST['nombre_usuario'];
 
 $email_usuario = $_POST['email_usuario'];
 
 include 'db/conexion.php';
 
-$consulta = "insert into usuarios values(NULL, '$nombre_usuario', '$email_usuario')";
 
-mysqli_query($conexion,$consulta);
+// sql para insertar usuario
+$insertar_usuario = "insert into usuarios values(NULL, '$nombre_usuario', '$email_usuario')";
+// ejecuto insercion del usuario
+mysqli_query($conexion,$insertar_usuario);
+// obtengo el id usuario
+$id_usuario = mysqli_insert_id($conexion);
 
 
-
-
+//sql para insertar pedidos
+$insertar_pedido = "insert into pedidos values (NULL, $id_usuario,NULL,'no')";
+// ejecuto insercion del pedido
+mysqli_query($conexion,$insertar_pedido);
+//obtengo el id del pedido
+$id_pedido = mysqli_insert_id($conexion);
+// saco por pantalla como prueba
+echo "Hola $nombre_usuario tienes un id  $id_usuario y tu numero de pedido es $id_pedido"
 
 
 ?>
